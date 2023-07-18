ef94ee63151f0b2431644db3493ac9d0f1c86e5c
0xpetra <franco.petraz@gmail.com>
2023-07-18 12:28:18 +0200

    📦 NEW: risc0 pseudocode for circuit

--- src-tauri/risczero/methods/guest/src/main.rs
+++ src-tauri/risczero/methods/guest/src/main.rs
@@ -14,20 +14,55 @@
 
 #![no_main]
 
+use hex_literal::hex;
+use hmac::{Hmac, Mac};
 use json::parse;
 use json_core::Outputs;
 use risc0_zkvm::{
   guest::env,
   sha::{Impl, Sha256},
 };
+use sha2::Sha256;
 
 risc0_zkvm::guest::entry!(main);
+
+pub async fn generate_xhmac_sig(content: &str) -> Result<String, Box<dyn Error>> {
+  dotenv().ok();
+  let shared_secret_key = env::var("TEST_VERIFF_SECRET").expect("TEST_VERIFF_SECRET must be set"); // This should be used to create the X-MAC-SIGNATURE
+
+  let mut mac = HmacSha256::new_from_slice(shared_secret_key.as_bytes())
+    .expect("HMAC can take key of any size");
+
+  mac.update(content.as_bytes());
+  // `result` has type `CtOutput` which is a thin wrapper around array of
+  // bytes for providing constant time equality check
+  let result = mac.finalize();
+  let hash = hex::encode(result.into_bytes());
+
+  let x_hmac_signature = format!("sha256={}", hash);
+
+  println!("x_hmac_signature: {}", x_hmac_signature);
+
+  Ok(format!("{}", x_hmac_signature))
+}
 
 pub fn main() {
   let data: String = env::read();
   let sha = *Impl::hash_bytes(&data.as_bytes());
   let data = parse(&data).unwrap();
-  let proven_val = data["firstName"].as_u32().unwrap();
+
+  // xhmac provided by veriff.com on the header's response
+  let xhmac = data["xhmac"].as_u32().unwrap();
+
+  // payload containing the user's data
+  let input = data["payload"].as_string().unwrap();
+  let sig = generate_xhmac_sig(&payload);
+
+  // We check integrity and authenticity,
+  assert_eq!(xhmac, sig);
+
+  // if the check passes we create the proof making firstName public
+  let proven_val = data["payload"]["firstName"].as_u32().unwrap();
   let out = Outputs {
     data: proven_val,
     hash: sha,
